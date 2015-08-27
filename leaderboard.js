8230510dae0f3125530107bdf21be3b73eefab0b
alexnicolini <alexnicolini@gmail.com>
2015-08-27 14:36:28 -0300

    Added Template to display players

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,19 @@
 PlayersList = new Mongo.Collection('players'); // global variable
+
+if (Meteor.isClient) {
+
+  // this code only run on the client
+  Template.leaderboard.helpers({
+    // helper functions go here
+    'player': function () {
+      return PlayersList.find();
+    },
+    'count': function() {
+      return PlayersList.find().count();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  // this code only run on the server
+}
