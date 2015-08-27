59a8fb84f55a361e0579233f9f5c62cef2a55e8c
alexnicolini <alexnicolini@gmail.com>
2015-08-27 17:27:38 -0300

    Added effect to the selected player

--- leaderboard.js
+++ leaderboard.js
@@ -1,15 +1,29 @@
 PlayersList = new Mongo.Collection('players'); // global variable
 
 if (Meteor.isClient) {
-
   // this code only run on the client
+
   Template.leaderboard.helpers({
     // helper functions go here
     'player': function () {
       return PlayersList.find();
     },
+    'selectedClass': function() {
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if (playerId == selectedPlayer) {
+        return 'selected';
+      }
+    },
     'count': function() {
       return PlayersList.find().count();
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function () {
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
     }
   });
 }
